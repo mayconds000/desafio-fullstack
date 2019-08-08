48196c68ac3796e709b0ddc86b27d12718341cee
mayconds000 <mayconds000@gmail.com>
2019-08-08 05:50:56 -0300

    add login and register page to frontend

---
+++ frontend/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container col-md-6 mt-5">
+    <h2>Login</h2>
+    <br>
+    <form>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="email" class="form-control" aria-describedby="emailHelp">
+        <small class="form-text text-danger"></small>
+      </div>
+      <div class="form-group">
+        <label>Senha</label>
+        <input type="password" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">Entrar</button>
+    </form>
+    <br>
+    <p>Ainda não tem uma conta? <nuxt-link to="/register">Registre-se</nuxt-link></p>
+  </div>
+</template>
