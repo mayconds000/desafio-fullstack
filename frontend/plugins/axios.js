dc42ae239ebf71c9b9077754ac0395f74c1db7dd
mayconds000 <mayconds000@gmail.com>
2019-08-09 01:31:45 -0300

    fix global validation

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,9 +1,12 @@
 export default function ({$axios, store}) {
   $axios.onError(error => {
-    console.log(error)
+    if (error.response.status === 422) {
+      store.dispatch("validation/setErrors", error.response.data.errors)
+    }
+    return Promise.reject(error)
   })
 
   $axios.onRequest(() => {
-    console.log("teste on Request")
+    store.dispatch("validation/clearErrors")
   })
 }
